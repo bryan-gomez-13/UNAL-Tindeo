42385a094987c7fd421240f0194d44e159bafbbe
Carlos Andrés Vesga Salas <cavesa10@gmail.com>
2021-11-21 14:45:57 -0500

    Implementando fragmento seleccionar rol, navegación login a crear cuenta

--- app/src/main/java/com/tindeo/BryanGomezHincapie/AccountOptionsFragment.kt
+++ app/src/main/java/com/tindeo/BryanGomezHincapie/AccountOptionsFragment.kt
@@ -5,27 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import com.tindeo.BryanGomezHincapie.databinding.FragmentAccountOptionsBinding
 import com.tindeo.BryanGomezHincapie.databinding.FragmentLoginBinding
 
 
-class LoginFragment : Fragment() {
-    private var _binding: FragmentLoginBinding? = null
+class AccountOptionsFragment : Fragment() {
+    private var _binding: FragmentAccountOptionsBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        _binding = FragmentAccountOptionsBinding.inflate(inflater, container, false)
         return binding.root
-    }
-
-    override fun onStart() {
-        super.onStart()
-        binding.singUpButton.setOnClickListener{
-            findNavController().navigate(R.id.action_loginFragment_to_accountOptionsFragment)
-        }
     }
 
 
