4ffe2f340911914c2b781ed96c7466e8144068f2
bryan-gomez-13 <bryan.gh041399@gmail.com>
2021-11-24 15:35:39 -0500

    Correción del login

--- app/src/main/java/com/tindeo/BryanGomezHincapie/MainActivity.kt
+++ app/src/main/java/com/tindeo/BryanGomezHincapie/MainActivity.kt
@@ -2,15 +2,19 @@ package com.tindeo.BryanGomezHincapie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.ui.AppBarConfiguration
 import com.tindeo.BryanGomezHincapie.databinding.ActivityHomeBinding
+import com.tindeo.BryanGomezHincapie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityHomeBinding
+    private lateinit var  appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
