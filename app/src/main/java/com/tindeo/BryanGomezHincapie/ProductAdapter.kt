db1f7ae57fa4dd59926e62aff7c5bbd73f5c3e28
Carlos Andrés Vesga Salas <cavesa10@gmail.com>
2021-11-27 13:07:27 -0500

    Codificando ProductAdapter.kt

--- app/src/main/java/com/tindeo/BryanGomezHincapie/ProductAdapter.kt
+++ app/src/main/java/com/tindeo/BryanGomezHincapie/ProductAdapter.kt
@@ -1,4 +1,29 @@
 package com.tindeo.BryanGomezHincapie
 
-class ProductAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.tindeo.BryanGomezHincapie.databinding.ItemProductBinding
+
+class ProductAdapter (val items: List<Product>):RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
+    class ViewHolder(val item: ItemProductBinding): RecyclerView.ViewHolder(item.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemProductBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+        holder.item.itemProductName.text = item.name
+        holder.item.itemProductPrice.text = item.price
+        Glide.with(holder.itemView).load(item.image).into(holder.item.itemProductPhoto)
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
 }
