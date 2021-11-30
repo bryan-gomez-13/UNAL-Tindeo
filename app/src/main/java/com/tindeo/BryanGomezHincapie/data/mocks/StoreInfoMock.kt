88823bb9e59aadb1f3ad5b37032eeb175233aad7
Carlos Andrés Vesga Salas <cavesa10@gmail.com>
2021-11-30 18:23:57 -0500

    Implementando diseño fragment_home.xml

--- app/src/main/java/com/tindeo/BryanGomezHincapie/data/mocks/StoreInfoMock.kt
+++ app/src/main/java/com/tindeo/BryanGomezHincapie/data/mocks/StoreInfoMock.kt
@@ -6,9 +6,9 @@ class StoreInfoMock {
     fun loadInfo(): StoreInfo {
         return StoreInfo(
             "1",
-            "Mi tienda",
-            "https://cdn.pixabay.com/photo/2016/03/22/09/08/online-store-1272390_960_720.png",
-            "calle Falsa 123",
+            "La curiteña",
+            "https://cdn.pixabay.com/photo/2014/04/02/13/07/choctaw-bluff-305932_960_720.jpg",
+            "Calle 20 #10-15 Bogotá",
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             null,
             null,
