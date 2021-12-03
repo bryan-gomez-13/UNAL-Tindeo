92f5bc21efc141df2f49cfc47b9eb6b5827b7ba5
Carlos Andrés Vesga Salas <cavesa10@gmail.com>
2021-12-02 21:55:25 -0500

    Habilitando los permisos de tomar fotos con la camara desde ProfileFragment.kt

--- app/src/main/java/com/tindeo/BryanGomezHincapie/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/tindeo/BryanGomezHincapie/ui/fragments/ProfileFragment.kt
@@ -1,17 +1,30 @@
 package com.tindeo.BryanGomezHincapie.ui.fragments
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.tindeo.BryanGomezHincapie.databinding.FragmentProfileBinding
+import java.lang.Exception
 
 
 class ProfileFragment : Fragment() {
 
     private var _binding:FragmentProfileBinding? = null
     private val binding get() = _binding!!
+
+    private val REQUEST_CAMERA_PERMISSION = 1
+    private val REQUEST_IMAGE = 2
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,5 +33,39 @@ class ProfileFragment : Fragment() {
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        checkPermission()
+        events()
+    }
+
+    private fun events() {
+        binding.profileImage.setOnClickListener{
+            if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA ) == PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.CAMERA),REQUEST_CAMERA_PERMISSION)
+                Intent(MediaStore.ACTION_IMAGE_CAPTURE).also{ intent->
+                    try{
+                        startActivityForResult(intent,REQUEST_IMAGE)
+                    }catch (e: Exception){
+
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK){
+            if (requestCode == REQUEST_IMAGE){
+                val bitmap = data?.extras?.get("data") as Bitmap
+                binding.profileImage.setImageBitmap(bitmap)
+            }
+        }
+    }
+    private fun checkPermission () {
+        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA ) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.CAMERA),REQUEST_CAMERA_PERMISSION)
+        }
+    }
 
 }
